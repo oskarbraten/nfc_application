0b50845d7be54f130a7b2e2d5f2020d7ec0a7f2f
Oskar Bråten <oskarbraten@users.noreply.github.com>
2018-02-13 13:42:51 +0100

    Created PayActivity.

---
+++ app/src/main/java/no/hvl/dat153/nfc_project/PayActivity.java
@@ -0,0 +1,13 @@
+package no.hvl.dat153.nfc_project;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PayActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pay);
+    }
+}
