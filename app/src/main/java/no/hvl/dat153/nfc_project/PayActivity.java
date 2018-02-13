3a201d4163db0bf755e5de8f4bbe2e1c18e2870b
Daniel Moberg <daniel@moberg.no>
2018-02-13 21:26:10 +0100

    småting

--- app/src/main/java/no/hvl/dat153/nfc_project/PayActivity.java
+++ app/src/main/java/no/hvl/dat153/nfc_project/PayActivity.java
@@ -1,13 +1,73 @@
 package no.hvl.dat153.nfc_project;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.NfcAdapter;
+import android.nfc.NfcAdapter.CreateNdefMessageCallback;
+import android.nfc.NfcEvent;
 import android.os.Bundle;
+import android.os.Parcelable;
+import android.widget.TextView;
+import android.widget.Toast;
+import java.nio.charset.Charset;
 
-public class PayActivity extends Activity {
+import static android.nfc.NdefRecord.createMime;
+
+
+public class PayActivity extends Activity implements CreateNdefMessageCallback {
+
+    NfcAdapter mNfcAdapter;
+    TextView textView;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pay);
+        //textView = (TextView) findViewById(R.id.textView);
+
+        mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        if (mNfcAdapter == null) {
+            Toast.makeText(this, "NFC is not available", Toast.LENGTH_LONG).show();
+            finish();
+            return;
+        }
+
+        mNfcAdapter.setNdefPushMessageCallback(this, this);
     }
+
+    @Override
+    public NdefMessage createNdefMessage(NfcEvent event) {
+        String text = ("kos på os");
+        NdefMessage msg = new NdefMessage(
+                new NdefRecord[] { createMime(
+                        "application/no.hvl.dat153.nfc_project", text.getBytes())
+                        //,NdefRecord.createApplicationRecord("no.hvl.dat153.nfc_project")
+                });
+        return msg;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(getIntent().getAction())) {
+            processIntent(getIntent());
+        }
+    }
+
+    @Override
+    public void onNewIntent(Intent intent) {
+        setIntent(intent);
+    }
+
+    void processIntent(Intent intent) {
+        textView = (TextView) findViewById(R.id.textView);
+        Parcelable[] rawMsgs = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+
+        NdefMessage msg = (NdefMessage) rawMsgs[0];
+
+        textView.setText(new String(msg.getRecords()[0].getPayload()));
+    }
+
 }
