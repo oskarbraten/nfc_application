3affa183f8a3892776a04d4134751e91e5324b6c
oskarbraten <oskar.braten@outlook.com>
2018-02-13 20:54:29 +0100

    Created button to PayActivity. Bumped target java version to 1.8 for lambda support.

--- app/src/main/java/no/hvl/dat153/nfc_project/MainActivity.java
+++ app/src/main/java/no/hvl/dat153/nfc_project/MainActivity.java
@@ -1,13 +1,41 @@
 package no.hvl.dat153.nfc_project;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.nfc.NfcAdapter;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+
+    public static final String MIME_TEXT_PLAIN = "text/plain";
+    private NfcAdapter nfcAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+
+        if (nfcAdapter == null) {
+            Toast.makeText(this, "This device does not support NFC.", Toast.LENGTH_LONG).show();
+            finish();
+            return;
+        }
+
+        if (!nfcAdapter.isEnabled()) {
+            Toast.makeText(this, "You need to enable NFC to send/receive payments.", Toast.LENGTH_LONG).show();
+        }
+
+        Button paymentBtn = findViewById(R.id.paymentBtn);
+        paymentBtn.setOnClickListener((View v) -> {
+            startActivity(new Intent(this, PayActivity.class));
+        });
     }
 }
