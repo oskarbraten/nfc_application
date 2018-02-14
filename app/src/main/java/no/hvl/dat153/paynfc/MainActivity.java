a6a698020b267c60a8ed7cac033e7a1dbd242dc0
oskarbraten <oskar.braten@outlook.com>
2018-02-14 16:09:13 +0100

    Started adding foregroundDispatcher.

--- app/src/main/java/no/hvl/dat153/paynfc/MainActivity.java
+++ app/src/main/java/no/hvl/dat153/paynfc/MainActivity.java
@@ -1,7 +1,9 @@
 package no.hvl.dat153.paynfc;
 
 import android.app.Activity;
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.nfc.NdefMessage;
 import android.nfc.NfcAdapter;
@@ -15,7 +17,7 @@ import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
-    //public static String MIME_TYPE = "application/no.hvl.dat153.nfc_project";
+    public static String MIME_TYPE = "application/no.hvl.dat153.nfc_project";
     private NfcAdapter nfcAdapter;
     private TextView balanceLabel;
     private EditText amountField;
@@ -117,5 +119,31 @@ public class MainActivity extends Activity {
         balanceLabel.setText(String.valueOf(newBalance));
 
         Toast.makeText(this, "Received " + String.valueOf(amount), Toast.LENGTH_LONG).show();
+    }
+
+    public static void startForegroundDispatch(final Activity activity, NfcAdapter adapter) {
+        final Intent intent = new Intent(activity.getApplicationContext(), activity.getClass());
+        intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+
+        final PendingIntent pendingIntent = PendingIntent.getActivity(activity.getApplicationContext(), 0, intent, 0);
+
+        IntentFilter[] filters = new IntentFilter[1];
+        String[][] techList = new String[][]{};
+
+        // Notice that this is the same filter as in our manifest.
+        filters[0] = new IntentFilter();
+        filters[0].addAction(NfcAdapter.ACTION_NDEF_DISCOVERED);
+        filters[0].addCategory(Intent.CATEGORY_DEFAULT);
+        try {
+            filters[0].addDataType(MIME_TYPE);
+        } catch (IntentFilter.MalformedMimeTypeException e) {
+            throw new RuntimeException("Check your mime type.");
+        }
+
+        adapter.enableForegroundDispatch(activity, pendingIntent, filters, techList);
+    }
+
+    public static void stopForegroundDispatch(final Activity activity, NfcAdapter adapter) {
+        adapter.disableForegroundDispatch(activity);
     }
 }
