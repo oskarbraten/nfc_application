175a14b2a1c58b58f77bb17acb4c462b647f5eb0
oskarbraten <oskar.braten@outlook.com>
2018-02-14 16:15:08 +0100

    Completed foregroundDispatcher.

--- app/src/main/java/no/hvl/dat153/paynfc/MainActivity.java
+++ app/src/main/java/no/hvl/dat153/paynfc/MainActivity.java
@@ -42,23 +42,17 @@ public class MainActivity extends Activity {
             Toast.makeText(this, "You need to enable NFC to send/receive payments.", Toast.LENGTH_LONG).show();
         }
 
-        // Check to see that the Activity started due to an Android Beam
-        // and that the intent has not been parced before
-        if (savedInstanceState == null && NfcAdapter.ACTION_NDEF_DISCOVERED.equals(getIntent().getAction())) {
-            processIntent(getIntent());
-        }
-
         Button paymentBtn = findViewById(R.id.paymentBtn);
         paymentBtn.setOnClickListener((View v) -> {
 
             String amountString = amountField.getText().toString();
 
-            if (amountString == null || amountString.isEmpty()) {
+            if (amountString.isEmpty()) {
                 Toast.makeText(this, "Please enter an amount.", Toast.LENGTH_SHORT).show();
                 return;
             }
 
-            int amount = Integer.parseInt(amountString.toString());
+            int amount = Integer.parseInt(amountString);
             final int currentBalance = getSharedPreferences("app_preferences", MODE_PRIVATE).getInt("balance", 100);
 
             if (amount < 0) {
@@ -77,23 +71,12 @@ public class MainActivity extends Activity {
         addCurrencyBtn.setOnClickListener((View v) -> {
             startActivity(new Intent(this, AddGrunkerActivity.class));
         });
-    }
 
-
-    @Override
-    public void onResume() {
-        super.onResume();
-
-        // update balanceLabel
-        final int currentBalance = getSharedPreferences("app_preferences", MODE_PRIVATE).getInt("balance", 100);
-        balanceLabel.setText(String.valueOf(currentBalance));
-
-        // clear amountField.
-        amountField.setText("");
-        amountField.clearFocus();
-
-        // set message to null to avoid resending NDEF payload.
-        nfcAdapter.setNdefPushMessage(null, this);
+        // Check to see that the Activity started due to an Android Beam
+        // and that the intent has not been parsed before
+        if (savedInstanceState == null && NfcAdapter.ACTION_NDEF_DISCOVERED.equals(getIntent().getAction())) {
+            processIntent(getIntent());
+        }
     }
 
     private void processIntent(Intent intent) {
@@ -122,6 +105,31 @@ public class MainActivity extends Activity {
         Toast.makeText(this, "Received " + String.valueOf(amount), Toast.LENGTH_LONG).show();
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        // update balanceLabel
+        final int currentBalance = getSharedPreferences("app_preferences", MODE_PRIVATE).getInt("balance", 100);
+        balanceLabel.setText(String.valueOf(currentBalance));
+
+        // clear amountField.
+        amountField.setText("");
+        amountField.clearFocus();
+
+        // set message to null to avoid resending NDEF payload.
+        nfcAdapter.setNdefPushMessage(null, this);
+
+        startForegroundDispatch(this, nfcAdapter);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        stopForegroundDispatch(this, nfcAdapter);
+    }
+
     public static void startForegroundDispatch(final Activity activity, NfcAdapter adapter) {
         final Intent intent = new Intent(activity.getApplicationContext(), activity.getClass());
         intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
@@ -146,5 +154,10 @@ public class MainActivity extends Activity {
 
     public static void stopForegroundDispatch(final Activity activity, NfcAdapter adapter) {
         adapter.disableForegroundDispatch(activity);
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        processIntent(intent);
     }
 }
