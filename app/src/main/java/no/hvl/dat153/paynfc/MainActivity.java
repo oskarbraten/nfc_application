6ec41aaae959ab273cc1174e292645fd8dda0035
oskarbraten <oskar.braten@outlook.com>
2018-02-14 15:00:45 +0100

    Implemented changing balance on incoming funds.

--- app/src/main/java/no/hvl/dat153/paynfc/MainActivity.java
+++ app/src/main/java/no/hvl/dat153/paynfc/MainActivity.java
@@ -1,31 +1,23 @@
 package no.hvl.dat153.paynfc;
 
 import android.app.Activity;
-import android.app.PendingIntent;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.nfc.NdefMessage;
-import android.nfc.NdefRecord;
 import android.nfc.NfcAdapter;
-import android.nfc.Tag;
-import android.nfc.tech.Ndef;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Parcelable;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.io.UnsupportedEncodingException;
-import java.util.Arrays;
+import static java.lang.Integer.parseInt;
 
 public class MainActivity extends Activity {
 
-    public static String MIME_TYPE = "application/no.hvl.dat153.nfc_project";
+    //public static String MIME_TYPE = "application/no.hvl.dat153.nfc_project";
     private NfcAdapter nfcAdapter;
     private TextView balanceLabel;
 
@@ -34,34 +26,16 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        SharedPreferences appPref = getSharedPreferences("app_preferences", MODE_PRIVATE);
-
-        final int currentBalance = appPref.getInt("balance", 100);
+        final int currentBalance = getSharedPreferences("app_preferences", MODE_PRIVATE).getInt("balance", 100);
 
         balanceLabel = findViewById(R.id.balanceLabel);
         balanceLabel.setText(Integer.toString(currentBalance));
-
-
-        nfcAdapter = NfcAdapter.getDefaultAdapter(this);
-
-        nfcAdapter.setNdefPushMessage(null, this);
-
-        if (nfcAdapter == null) {
-            Toast.makeText(this, "This device does not support NFC.", Toast.LENGTH_LONG).show();
-            finish();
-            return;
-        }
-
-        if (!nfcAdapter.isEnabled()) {
-            Toast.makeText(this, "You need to enable NFC to send/receive payments.", Toast.LENGTH_LONG).show();
-        }
 
         Button paymentBtn = findViewById(R.id.paymentBtn);
         paymentBtn.setOnClickListener((View v) -> {
             EditText amountField = findViewById(R.id.amountField);
 
-
-            int amount = Integer.parseInt(amountField.getText().toString());
+            int amount = parseInt(amountField.getText().toString());
 
             if (amount < 0) {
                 Toast.makeText(this, "Invalid amount.", Toast.LENGTH_SHORT).show();
@@ -80,28 +54,46 @@ public class MainActivity extends Activity {
     @Override
     public void onResume() {
         super.onResume();
+
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+
+        if (nfcAdapter == null) {
+            Toast.makeText(this, "This device does not support NFC.", Toast.LENGTH_LONG).show();
+            finish();
+            return;
+        }
+
+        if (!nfcAdapter.isEnabled()) {
+            Toast.makeText(this, "You need to enable NFC to send/receive payments.", Toast.LENGTH_LONG).show();
+        }
+
+        // set message to null to avoid resending NDEF payload.
+        nfcAdapter.setNdefPushMessage(null, this);
+
         // Check to see that the Activity started due to an Android Beam
         if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(getIntent().getAction())) {
             processIntent(getIntent());
         }
     }
 
-//    @Override
-//    public void onNewIntent(Intent intent) {
-//        // onResume gets called after this to handle the intent
-//        setIntent(intent);
-//    }
-
     /**
      * Parses the NDEF Message from the intent and prints to the TextView
      */
     void processIntent(Intent intent) {
-        Parcelable[] rawMsgs = intent.getParcelableArrayExtra(
-                NfcAdapter.EXTRA_NDEF_MESSAGES);
+        Parcelable[] rawMessages = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
         // only one message sent during the beam
-        NdefMessage msg = (NdefMessage) rawMsgs[0];
-        // record 0 contains the MIME type, record 1 is the AAR, if present
+        NdefMessage message = (NdefMessage) rawMessages[0];
 
-        Log.d("TESAT: ", new String(msg.getRecords()[0].getPayload()));
+        SharedPreferences prefs = getSharedPreferences("app_preferences", MODE_PRIVATE);
+
+        // record 0 contains the MIME type, record 1 is the AAR, if present
+        final int amount = Integer.parseInt(new String(message.getRecords()[0].getPayload()));
+        final int currentBalance = prefs.getInt("balance", 100);
+
+        final int newBalance = currentBalance + amount;
+
+        // update preferences, and label.
+        prefs.edit().putInt("balance", newBalance).apply();
+        balanceLabel.setText(String.valueOf(newBalance));
     }
 }
