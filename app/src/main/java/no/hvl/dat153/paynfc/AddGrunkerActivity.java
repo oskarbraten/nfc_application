4ffc180eca34dafbecff7826d3ba97405fbff2c0
Daniel Moberg <daniel@moberg.no>
2018-02-14 16:05:43 +0100

    AddGrunker lagt til(java)

---
+++ app/src/main/java/no/hvl/dat153/paynfc/AddGrunkerActivity.java
@@ -0,0 +1,13 @@
+package no.hvl.dat153.paynfc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddGrunkerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_grunker);
+    }
+}
